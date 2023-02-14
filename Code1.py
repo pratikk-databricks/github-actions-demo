93b37149d678155441d8507f18e48d1fd796edbe
pratikk-databricks <pratikk-databricks>
2023-02-14 13:52:29 +0000

    adding comment

--- Code1.py
+++ Code1.py
@@ -5,6 +5,7 @@ from pyspark.sql import DataFrame, SparkSession
 
 # COMMAND ----------
 
+# generating test data
 def generate_data1(n=1000, name='my_cool_data'):
     df = SparkSession.getActiveSession().range(0, n)
     df.createOrReplaceTempView(name)
